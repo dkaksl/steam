a18b6fc35d0266dec465ee734724966d1626b2ab
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-04 21:49:36 +0900

    created more packages

--- src/main/java/com/kilumanga/play/steam/steam_news/ISteamNews.java
+++ src/main/java/com/kilumanga/play/steam/steam_news/ISteamNews.java
@@ -5,7 +5,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-package com.kilumanga.play.steam;
+package com.kilumanga.play.steam.steam_news;
 
 /**
  * @author Amani
