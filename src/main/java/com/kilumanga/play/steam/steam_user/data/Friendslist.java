4f4865482a2faab18dc41aa65b07a537395e886b
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-04 23:33:37 +0900

    added friendslist pojo

---
+++ src/main/java/com/kilumanga/play/steam/steam_user/data/Friendslist.java
@@ -0,0 +1,26 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.steam_user.data;
+
+import java.util.List;
+
+/**
+ * @author Amani
+ *
+ */
+public class Friendslist {
+	private List<Friend> friends;
+
+	public List<Friend> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(List<Friend> friends) {
+		this.friends = friends;
+	}
+}
