760c803cc6fdbe6540dabe29b5f7a0151980f09d
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-04 21:13:56 +0900

    added class skeletons for steam web api

---
+++ src/main/java/com/kilumanga/play/steam/ISteamUserStats.java
@@ -0,0 +1,16 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam;
+
+/**
+ * @author Amani
+ *
+ */
+public class ISteamUserStats {
+
+}
