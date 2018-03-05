53abae6cc1276be20330733a59819269e9e8dd5e
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-06 00:51:30 +0900

    added pojo for getplayersummaries method response

---
+++ src/main/java/com/kilumanga/play/steam/i_steam_user/summaries/GetPlayerSummariesResponse.java
@@ -0,0 +1,24 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.i_steam_user.summaries;
+
+/**
+ * @author Amani
+ *
+ */
+public class GetPlayerSummariesResponse {
+	private Response response;
+
+	public Response getResponse() {
+		return response;
+	}
+
+	public void setResponse(Response response) {
+		this.response = response;
+	}
+}
