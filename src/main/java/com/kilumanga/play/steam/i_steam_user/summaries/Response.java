df249edd910315c176ea7e4176bc83440cf89d5d
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-06 00:52:00 +0900

    added pojo for nested getplayersummaries response

---
+++ src/main/java/com/kilumanga/play/steam/i_steam_user/summaries/Response.java
@@ -0,0 +1,26 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.i_steam_user.summaries;
+
+import java.util.List;
+
+/**
+ * @author Amani
+ *
+ */
+public class Response {
+	private List<Player> players;
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+}
