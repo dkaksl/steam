9d7cc1db838eece4909f66defaa5b422055a2c52
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-05 23:00:28 +0900

    added api key value object

---
+++ src/main/java/com/kilumanga/play/steam/secret/ApiKey.java
@@ -0,0 +1,32 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.secret;
+
+import com.kilumanga.play.steam.constant.ExceptionMessage;
+
+/**
+ * @author Amani
+ *
+ */
+public class ApiKey {
+	private final String key;
+
+	public ApiKey(String key) {
+		if (key == null) {
+			throw new IllegalArgumentException(ExceptionMessage.NULL_PARAMETER.getExceptionMessage());
+		}
+		if (key.isEmpty()) {
+			throw new IllegalArgumentException(ExceptionMessage.INVALID_API_KEY.getExceptionMessage());
+		}
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+}
