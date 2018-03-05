2be0b14643a410510082c2c7f990ab7ce16743fd
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-06 00:51:00 +0900

    renamed packages

--- src/main/java/com/kilumanga/play/steam/i_steam_user/friends/Friendslist.java
+++ src/main/java/com/kilumanga/play/steam/i_steam_user/friends/Friendslist.java
@@ -5,7 +5,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-package com.kilumanga.play.steam.i_steam_user.data;
+package com.kilumanga.play.steam.i_steam_user.friends;
 
 import java.util.List;
 
