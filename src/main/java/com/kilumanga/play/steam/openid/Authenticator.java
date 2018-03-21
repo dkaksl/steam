614bf043ee9268ed1f175245640b4292d497826d
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-21 22:45:34 +0900

    added response verification method, moved variable initialization to constructor

--- src/main/java/com/kilumanga/play/steam/openid/Authenticator.java
+++ src/main/java/com/kilumanga/play/steam/openid/Authenticator.java
@@ -7,12 +7,18 @@
  */
 package com.kilumanga.play.steam.openid;
 
+import java.util.Map;
+
+import org.openid4java.association.AssociationException;
 import org.openid4java.consumer.ConsumerException;
 import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
 import org.openid4java.discovery.DiscoveryException;
 import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
 import org.openid4java.message.AuthRequest;
 import org.openid4java.message.MessageException;
+import org.openid4java.message.ParameterList;
 
 import com.kilumanga.play.steam.constant.ExceptionMessage;
 import com.kilumanga.play.steam.constant.Uri;
@@ -23,12 +29,14 @@ import com.kilumanga.play.steam.constant.Uri;
  */
 public class Authenticator {
 	private final ConsumerManager consumerManager = new ConsumerManager();
+	private final DiscoveryInformation discoveryInformation;
 
-	public Authenticator() {
+	public Authenticator() throws DiscoveryException {
 		consumerManager.setMaxAssocAttempts(0);
+		discoveryInformation = consumerManager.associate(consumerManager.discover(Uri.STEAM_OPENID.getUri()));
 	}
 
-	public String getLoginUrl(String callbackUrl) throws DiscoveryException, MessageException, ConsumerException {
+	public String getLoginUrl(String callbackUrl) throws MessageException, ConsumerException {
 		if (callbackUrl == null) {
 			throw new IllegalArgumentException(ExceptionMessage.NULL_PARAMETER.getExceptionMessage());
 		}
@@ -36,10 +44,24 @@ public class Authenticator {
 		if (callbackUrl.isEmpty()) {
 			throw new IllegalArgumentException(ExceptionMessage.INVALID_CALLBACK_URL.getExceptionMessage());
 		}
-
-		DiscoveryInformation discoveryInformation = consumerManager
-				.associate(consumerManager.discover(Uri.STEAM_OPENID.getUri()));
 		AuthRequest authRequest = consumerManager.authenticate(discoveryInformation, callbackUrl);
 		return authRequest.getDestinationUrl(true);
+	}
+
+	public String getVerifiedSteamId(String callbackUrl, Map<String, String> responseMap)
+			throws MessageException, DiscoveryException, AssociationException {
+		ParameterList parameterList = new ParameterList(responseMap);
+		VerificationResult verificationResult = consumerManager.verify(callbackUrl, parameterList,
+				this.discoveryInformation);
+		Identifier verifiedId = verificationResult.getVerifiedId();
+		if (verifiedId == null) {
+			throw new IllegalArgumentException(ExceptionMessage.NULL_PARAMETER.getExceptionMessage());
+		}
+		return extractSteamId(verifiedId.getIdentifier());
+	}
+
+	private String extractSteamId(String identifier) {
+		System.out.println(identifier);
+		return identifier;
 	}
 }
