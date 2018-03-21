4ebef57afd4c9b828b259ec81080485a85e8c0ab
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-21 20:56:34 +0900

    added player service test class

---
+++ src/test/java/com/kilumanga/play/steam/i_player_service/IPlayerServiceTest.java
@@ -0,0 +1,54 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.i_player_service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.kilumanga.play.steam.constant.ID;
+import com.kilumanga.play.steam.i_steam_user.ISteamUserTest;
+import com.kilumanga.play.steam.secret.ApiKey;
+
+/**
+ * @author Amani
+ *
+ */
+public class IPlayerServiceTest {
+	private static ApiKey apiKey;
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+
+	@BeforeClass
+	public static void setUp() throws IOException, URISyntaxException {
+		try (BufferedReader apiKeyReader = new BufferedReader(new InputStreamReader(new FileInputStream(
+				new File(ISteamUserTest.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
+						.getParentFile().getParentFile()
+						+ "/secret/apikey".replaceAll("/", Matcher.quoteReplacement(File.separator))),
+				StandardCharsets.UTF_8))) {
+			apiKey = new ApiKey(apiKeyReader.readLine());
+		}
+	}
+
+	@Test
+	public void testGetFriendList() {
+		IPlayerService iPlayerService = new IPlayerService(apiKey);
+		System.out.println(iPlayerService.getOwnedGames(ID.DEFAULT.getUserId()));
+	}
+}
