c4bd5d57f3666b98d6e27d8e0945e91ef4822697
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-21 20:24:52 +0900

    added openid authenticator

---
+++ src/main/java/com/kilumanga/play/steam/openid/Authenticator.java
@@ -0,0 +1,36 @@
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package com.kilumanga.play.steam.openid;
+
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.MessageException;
+
+import com.kilumanga.play.steam.constant.Uri;
+
+/**
+ * @author Amani
+ *
+ */
+public class Authenticator {
+	private final ConsumerManager consumerManager = new ConsumerManager();
+
+	public Authenticator() {
+		consumerManager.setMaxAssocAttempts(0);
+	}
+
+	public String getLoginUrl(String callbackUrl) throws DiscoveryException, MessageException, ConsumerException {
+		DiscoveryInformation discoveryInformation = consumerManager
+				.associate(consumerManager.discover(Uri.STEAM_OPENID.getUri()));
+		AuthRequest authRequest = consumerManager.authenticate(discoveryInformation, callbackUrl);
+		return authRequest.getDestinationUrl(true);
+	}
+}
