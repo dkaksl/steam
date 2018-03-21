fd93a2c69dfb154eac0ef0feb7b871a4d8779065
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-21 20:41:20 +0900

    renamed package to match main package

--- src/test/java/com/kilumanga/play/steam/i_steam_user/ISteamUserTest.java
+++ src/test/java/com/kilumanga/play/steam/i_steam_user/ISteamUserTest.java
@@ -5,7 +5,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-package com.kilumanga.play.steam.steam_user;
+package com.kilumanga.play.steam.i_steam_user;
 
 import java.io.BufferedReader;
 import java.io.File;
