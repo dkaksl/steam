a8bc7de9ff8d51d5c8eb0a1b511b660e2614a2c5
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-21 21:27:54 +0900

    added unit tests for authenticator parameter checks

--- src/test/java/com/kilumanga/play/steam/openid/AuthenticatorTest.java
+++ src/test/java/com/kilumanga/play/steam/openid/AuthenticatorTest.java
@@ -14,6 +14,8 @@ import org.openid4java.consumer.ConsumerException;
 import org.openid4java.discovery.DiscoveryException;
 import org.openid4java.message.MessageException;
 
+import com.kilumanga.play.steam.constant.ExceptionMessage;
+
 /**
  * @author Amani
  *
@@ -26,6 +28,22 @@ public class AuthenticatorTest {
 	public void test() throws DiscoveryException, MessageException, ConsumerException {
 		Authenticator authenticator = new Authenticator();
 		System.out.println(authenticator.getLoginUrl("https://steam.kilumanga.com"));
+	}
+
+	@Test
+	public void testNullCallback() throws DiscoveryException, MessageException, ConsumerException {
+		Authenticator authenticator = new Authenticator();
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(ExceptionMessage.NULL_PARAMETER.getExceptionMessage());
+		authenticator.getLoginUrl(null);
+	}
+
+	@Test
+	public void testBlankCallback() throws DiscoveryException, MessageException, ConsumerException {
+		Authenticator authenticator = new Authenticator();
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(ExceptionMessage.INVALID_CALLBACK_URL.getExceptionMessage());
+		authenticator.getLoginUrl("");
 	}
 
 }
