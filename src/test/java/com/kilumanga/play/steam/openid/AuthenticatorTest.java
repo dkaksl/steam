b6c51355dab07938e2acc23cc39cfa643fbc2ee9
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-10-07 03:13:48 +0800

    formatting; changed test to use an actual assert

--- src/test/java/com/kilumanga/play/steam/openid/AuthenticatorTest.java
+++ src/test/java/com/kilumanga/play/steam/openid/AuthenticatorTest.java
@@ -1,12 +1,12 @@
-/** 
- * 
+/**
  * Copyright (C) 2018 Amani
- *
+ * <p>
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
 package com.kilumanga.play.steam.openid;
 
+import com.kilumanga.play.steam.openid.data.CallbackUrl;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,12 +16,10 @@ import org.openid4java.consumer.ConsumerException;
 import org.openid4java.discovery.DiscoveryException;
 import org.openid4java.message.MessageException;
 
-import com.kilumanga.play.steam.constant.ExceptionMessage;
-import com.kilumanga.play.steam.openid.data.CallbackUrl;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Amani
- *
  */
 public class AuthenticatorTest {
     private static final CallbackUrl callbackUrl = new CallbackUrl("https://steam.kilumanga.com");
@@ -36,8 +34,8 @@ public class AuthenticatorTest {
     public final ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void testRequestRedirect() throws MessageException, ConsumerException {
-        System.out.println(authenticator.getLoginUrl());
+    public void testGetLoginUrl() throws MessageException, ConsumerException {
+        assertEquals("https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.return_to=https%3A%2F%2Fsteam.kilumanga.com&openid.realm=https%3A%2F%2Fsteam.kilumanga.com&openid.mode=checkid_setup", authenticator.getLoginUrl());
     }
 
     @Test
