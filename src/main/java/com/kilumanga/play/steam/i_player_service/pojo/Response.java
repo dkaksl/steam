b50f3d0d1df6045c23b9fb07a102ecc07387f5f2
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-10-07 17:07:19 +0800

    formatting; changed return type of nullable values to optional

--- src/main/java/com/kilumanga/play/steam/i_player_service/pojo/Response.java
+++ src/main/java/com/kilumanga/play/steam/i_player_service/pojo/Response.java
@@ -1,35 +1,34 @@
-/** 
- * 
+/**
  * Copyright (C) 2018 Amani
- *
+ * <p>
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
 package com.kilumanga.play.steam.i_player_service.pojo;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Amani
- *
  */
 public class Response {
-	private Integer gameCount;
-	private List<Game> games;
+    private Integer gameCount;
+    private List<Game> games;
 
-	public Integer getGameCount() {
-		return gameCount;
-	}
+    public Optional<Integer> getGameCount() {
+        return Optional.ofNullable(gameCount);
+    }
 
-	public void setGameCount(Integer gameCount) {
-		this.gameCount = gameCount;
-	}
+    public void setGameCount(Integer gameCount) {
+        this.gameCount = gameCount;
+    }
 
-	public List<Game> getGames() {
-		return games;
-	}
+    public Optional<List<Game>> getGames() {
+        return Optional.ofNullable(games);
+    }
 
-	public void setGames(List<Game> games) {
-		this.games = games;
-	}
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
 }
