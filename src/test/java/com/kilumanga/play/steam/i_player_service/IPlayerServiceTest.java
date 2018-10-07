94f99ff16185807ad098b41175ed42d411f0195e
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-10-07 17:09:48 +0800

    formatting; updated test methods to check optional values

--- src/test/java/com/kilumanga/play/steam/i_player_service/IPlayerServiceTest.java
+++ src/test/java/com/kilumanga/play/steam/i_player_service/IPlayerServiceTest.java
@@ -1,57 +1,64 @@
-/** 
- * 
+/**
  * Copyright (C) 2018 Amani
- *
+ * <p>
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
 package com.kilumanga.play.steam.i_player_service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-
+import com.kilumanga.play.steam.constant.ID;
+import com.kilumanga.play.steam.i_player_service.pojo.Game;
+import com.kilumanga.play.steam.i_player_service.pojo.GetOwnedGamesResponse;
+import com.kilumanga.play.steam.i_steam_user.ISteamUserTest;
+import com.kilumanga.play.steam.secret.ApiKey;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.kilumanga.play.steam.constant.ID;
-import com.kilumanga.play.steam.i_player_service.pojo.Game;
-import com.kilumanga.play.steam.i_steam_user.ISteamUserTest;
-import com.kilumanga.play.steam.secret.ApiKey;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
 
 /**
  * @author Amani
  *
  */
 public class IPlayerServiceTest {
-	private static ApiKey apiKey;
+    private static ApiKey apiKey;
 
-	@Rule
-	public final ExpectedException exception = ExpectedException.none();
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
-	@BeforeClass
-	public static void setUp() throws IOException, URISyntaxException {
-		try (BufferedReader apiKeyReader = new BufferedReader(new InputStreamReader(new FileInputStream(
-				new File(ISteamUserTest.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
-						.getParentFile().getParentFile()
-						+ "/secret/apikey".replaceAll("/", Matcher.quoteReplacement(File.separator))),
-				StandardCharsets.UTF_8))) {
-			apiKey = new ApiKey(apiKeyReader.readLine());
-		}
-	}
+    @BeforeClass
+    public static void setUp() throws IOException, URISyntaxException {
+        try (BufferedReader apiKeyReader = new BufferedReader(new InputStreamReader(new FileInputStream(
+                new File(ISteamUserTest.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
+                        .getParentFile().getParentFile()
+                        + "/secret/apikey".replaceAll("/", Matcher.quoteReplacement(File.separator))),
+                StandardCharsets.UTF_8))) {
+            apiKey = new ApiKey(apiKeyReader.readLine());
+        }
+    }
 
-	@Test
-	public void testGetOwnedGames() {
-		IPlayerService iPlayerService = new IPlayerService(apiKey);
-		for (Game game : iPlayerService.getOwnedGames(ID.DEFAULT.getUserId()).getResponse().getGames()) {
-			System.out.println("appid: " + game.getAppid() + ", playtime_forever: " + game.getPlaytime_forever());
-		}
-	}
+    @Test
+    public void testGetOwnedGames() {
+        IPlayerService iPlayerService = new IPlayerService(apiKey);
+        GetOwnedGamesResponse getOwnedGamesResponse = iPlayerService.getOwnedGames(ID.DEFAULT.getUserId());
+        Optional<Integer> gameCount = getOwnedGamesResponse.getResponse().getGameCount();
+        if (gameCount.isPresent()) {
+            System.out.println("game count: " + gameCount.get());
+        }
+
+        Optional<List<Game>> gameList = getOwnedGamesResponse.getResponse().getGames();
+        if (gameList.isPresent()) {
+            System.out.println("owned games:");
+            for (Game game : gameList.get()) {
+                System.out.println("appid: " + game.getAppid() + ", playtime_forever: " + game.getPlaytime_forever());
+            }
+        }
+    }
 }
